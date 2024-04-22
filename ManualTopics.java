34c86b117a9318275dc6fc78870d4f1506eb84ee
Rupesh Khatri <khatri_rupesh@yahoo.com>
2024-04-22 22:46:18 +0100

    my first commit

---
+++ ManualTopics.java
@@ -0,0 +1,45 @@
+package list_of_manual_topics;
+
+public class ManualTopics {
+
+    public static void main(String[] args){
+
+        System.out.println("Manual Testing Important Interview Questions ");
+        System.out.println("What is software?");
+        System.out.println("Software risks");
+        System.out.println("Benefits of software testing ");
+        System.out.println("Who does testing?");
+        System.out.println("Qualifications of a tester?" );
+        System.out.println("What is manual testing?");
+        System.out.println("What is SDLC");
+        System.out.println("What is STLC");
+        System.out.println("Challenges of manual testing");
+        System.out.println("Principals of software testing");
+        System.out.println("Test plan and it's benefits");
+        System.out.println("Test plan Template");
+        System.out.println("Test scenario+template");
+        System.out.println("Test case+template");
+        System.out.println("Test scenario vs test case");
+        System.out.println("Testing Techniques");
+        System.out.println("What is bug?");
+        System.out.println("Bug life cycle");
+        System.out.println("Bug template");
+        System.out.println("Severity vs Priority");
+        System.out.println("Testing levels");
+        System.out.println("White box testing - Black box testing");
+        System.out.println("Static vs Dynamic testing");
+        System.out.println("Exploratory testing");
+        System.out.println("Ad-Hoc testing");
+        System.out.println("Software development models");
+        System.out.println("Waterfall model");
+        System.out.println("Iterative model");
+        System.out.println("V-model)");
+        System.out.println("Verification amd validation");
+        System.out.println("Agile model");
+        System.out.println("Definition of Ready/Done)");
+        System.out.println();
+
+
+
+    }
+}
