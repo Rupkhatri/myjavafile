4b42e2d849560b40029adb9c5b77be43daf10abd
Rupkhatri <167824219+Rupkhatri@users.noreply.github.com>
2024-04-24 16:03:42 +0100

    Add files via upload

---
+++ MyFirstClass.java
@@ -0,0 +1,14 @@
+package a.myFirstPackage;
+
+public class MyFirstClass {
+
+
+    public static void main(String[] args){
+        System.out.println("Snehal");
+        System.out.println("Snehal");
+        System.out.println("Snehal");
+
+    }
+
+
+}
